e2ba104cca68f61b5d5ff1b179c42dde96df67e1
Krzysztof Jelski <Krzysztof Jelski@DESKTOP-2OHJH2M>
2016-05-27 21:08:26 +0200

    Add project files.

---
+++ clean-tests-kata/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace clean_tests_kata
+{
+    public class Class1
+    {
+    }
+}
